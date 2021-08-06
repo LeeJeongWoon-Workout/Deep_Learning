d14fd494b8f8c5d2630156a37e8f9cedb2a28b29
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-06 09:42:43 +0900

    Update and rename PyTorch_Practice/Torch_Layer.py to PyTorch_Practice/Torch_Layer/Setting.py

--- PyTorch_Practice/Torch_Layer/Setting.py
+++ PyTorch_Practice/Torch_Layer/Setting.py
@@ -16,24 +16,3 @@ image, label = next(iter(train_loader))
 image.shape, label.shape
 plt.imshow(image[0,0,:,:], 'gray')
 plt.show()
-
-
-#Layer
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-nn.Conv2d(in_channels=1, out_channels=20, kernel_size=5, stride=1)
-'''in_channels: input channel
-out_channels: output channel
-kernel_size: kernel(weights)'''
-
-layer = nn.Conv2d(1, 20, 5, 1).to(torch.device('gpu'))
-layer
-
-weight = layer.weight
-weight.shape
-'''torch.Size([20, 1, 5, 5])
-  [output_channel,input_channel,kernel_size()]
-'''
-
