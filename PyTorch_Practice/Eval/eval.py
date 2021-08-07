c8f7bb09e0a4951cd23d0d262f8558853b2947b4
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-07 13:07:39 +0900

    Create eval.py

---
+++ PyTorch_Practice/Eval/eval.py
@@ -0,0 +1,19 @@
+# 모델 평가 (test)
+model.eval()
+
+test_loss=0
+correct=0
+
+with torch.no_grad():
+  for data, target in test_loader:
+    data, target = data.to(device), target.to(device)
+    output = model(data)
+    test_loss += F.nll_loss(output, target, reduction='sum').item()
+    pred = output.argmax(dim=1, keepdim=True)
+    correct += pred.eq(target.view_as(pred)).sum().item()
+
+test_loss /= len(test_loader.dataset)
+
+print('\nTest set: Average Loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset), 100*correct/len(test_loader.dataset)
+))
